ee068ac8548d26303ff1b4479c2b128ea8fb9664
Justin Santa Barbara <justin@fathomdb.com>
2017-07-11 15:41:14 -0400

    Split out controller from etcd-runner

--- pkg/etcd/etcdprocess.go
+++ pkg/etcd/etcdprocess.go
@@ -1,46 +1,25 @@
 package etcd
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"os/exec"
 	"path"
 	"strings"
 	"sync"
 	"time"
-
+	protoetcd "kope.io/etcd-manager/pkg/apis/etcd"
 	"github.com/golang/glog"
 )
-
-type etcdProcessMember struct {
-	Id         string   `json:"id,omitempty"`
-	Name       string   `json:"name,omitempty"`
-	PeerURLs   []string `json:"peerURLs,omitempty"`
-	ClientURLs []string `json:"clientURLs,omitempty"`
-}
-
-func (m *etcdProcessMember) String() string {
-	s, err := json.Marshal(m)
-	if err != nil {
-		return fmt.Sprintf("<error marshallling: %v>", err)
-	}
-	return string(s)
-}
-
-type etcdProcessMemberList struct {
-	Members []*etcdProcessMember `json:"members"`
-}
 
 type etcdProcess struct {
 	BinDir  string
 	DataDir string
 
+	ClientURL string
+
 	CreateNewCluster bool
-	Version          string
+	//Version          string
 
 	//ClusterName string
 	//NodeName string
@@ -53,7 +32,7 @@ type etcdProcess struct {
 	//
 	//ClusterToken string
 
-	Cluster *EtcdCluster
+	Cluster *protoetcd.EtcdCluster
 
 	cmd *exec.Cmd
 
@@ -150,74 +129,4 @@ func (p *etcdProcess) Start() error {
 
 	p.cmd = c
 	return nil
-}
-
-func (e *etcdProcess) listMembers() ([]*etcdProcessMember, error) {
-	client := &http.Client{}
-	method := "GET"
-	url := fmt.Sprintf("http://127.0.0.1:%d/v2/members", e.Cluster.ClientPort)
-	request, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error building etcd request %s %s: %v", method, url, err)
-	}
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("error performing etcd request %s %s: %v", method, url, err)
-	}
-	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("unexpected response querying etcd members %s %s: %s", method, url, response.Status)
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading etcd response %s %s: %v", method, url, err)
-	}
-	members := &etcdProcessMemberList{}
-	if err := json.Unmarshal(body, &members); err != nil {
-		glog.Infof("invalid etcd response: %q", string(body))
-		return nil, fmt.Errorf("error parsing etcd response %s %s: %v", method, url, err)
-	}
-	return members.Members, nil
-}
-
-func (e *etcdProcess) addMember(name string, peerURLs []string) (*etcdProcessMember, error) {
-	client := &http.Client{}
-
-	m := &etcdProcessMember{
-		Name:     name,
-		PeerURLs: peerURLs,
-	}
-	postBody, err := json.Marshal(m)
-	if err != nil {
-		return nil, fmt.Errorf("error building payload for member-add: %v", err)
-	}
-	method := "POST"
-	url := fmt.Sprintf("http://127.0.0.1:%d/v2/members", e.Cluster.ClientPort)
-	request, err := http.NewRequest("POST", url, bytes.NewReader(postBody))
-	if err != nil {
-		return nil, fmt.Errorf("error building etcd request %s %s: %v", method, url, err)
-	}
-	request.Header.Add("Content-Type", "application/json")
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("error performing etcd request %s %s: %v", method, url, err)
-	}
-	defer response.Body.Close()
-	if response.StatusCode != 201 {
-		glog.Infof("POSTed content was %q", string(postBody))
-		return nil, fmt.Errorf("unexpected response adding etcd member %s %s: %s", method, url, response.Status)
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading etcd response %s %s: %v", method, url, err)
-	}
-	member := &etcdProcessMember{}
-	if err := json.Unmarshal(body, &member); err != nil {
-		glog.Infof("invalid etcd response: %q", string(body))
-		return nil, fmt.Errorf("error parsing etcd response %s %s: %v", method, url, err)
-	}
-	glog.Infof("created etcd member: %v", member)
-	return member, nil
 }
