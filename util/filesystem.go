f6ec99f686c4744f6aa2f55b4ffd11446b5aa200
Daniel Lipovetsky <daniel@platform9.com>
2018-06-14 14:29:01 -0700

    fix bug in FileExists

--- util/filesystem.go
+++ util/filesystem.go
@@ -6,7 +6,10 @@ import (
 
 // FileExists checks whether the file exists
 func FileExists(path string) (bool, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
 		return false, err
 	}
 	return true, nil
