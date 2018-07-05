abc854b4a7b7c549ee6a9bd0810250ec23fb3044
Arun Sriraman <sarun87@gmail.com>
2018-07-05 13:29:42 -0700

    Fix reset for single member cluster & fix etcdctl --version command for v3
    Changes:
    + Fixed etcdadm reset command to work for single node etcd cluster
    Issue: https://github.com/platform9/etcdadm/issues/14
    
    + Fixed etcdctl --version command for V3
    https://github.com/platform9/etcdadm/issues/15
    
    + Add --skip-remove-member flag
    https://github.com/platform9/etcdadm/issues/12
    
    + Refactor to return error rather than failing using log.Fatalf()

--- service/systemd.go
+++ service/systemd.go
@@ -12,6 +12,13 @@ func reloadSystemd() error {
 	return nil
 }
 
+func resetFailed() error {
+	if err := exec.Command("systemctl", "reset-failed").Run(); err != nil {
+		return fmt.Errorf("failed to reset failed systemd units: %v", err)
+	}
+	return nil
+}
+
 func serviceStart(service string) error {
 	// Before we try to start any service, make sure that systemd is ready
 	if err := reloadSystemd(); err != nil {
@@ -20,6 +27,18 @@ func serviceStart(service string) error {
 	args := []string{"start", service}
 	if err := exec.Command("systemctl", args...).Run(); err != nil {
 		return fmt.Errorf("failed to start service: %v", err)
+	}
+	return nil
+}
+
+func serviceStop(service string) error {
+	// Before we try to start any service, make sure that systemd is ready
+	if err := reloadSystemd(); err != nil {
+		return err
+	}
+	args := []string{"stop", service}
+	if err := exec.Command("systemctl", args...).Run(); err != nil {
+		return fmt.Errorf("failed to stop service: %v", err)
 	}
 	return nil
 }
@@ -36,10 +55,33 @@ func serviceEnable(service string) error {
 	return nil
 }
 
+func serviceDisable(service string) error {
+	// Before we try to disable any service, make sure that systemd is ready
+	if err := reloadSystemd(); err != nil {
+		return err
+	}
+	args := []string{"disable", service}
+	if err := exec.Command("systemctl", args...).Run(); err != nil {
+		return fmt.Errorf("failed to disable service: %v", err)
+	}
+	return nil
+}
+
 // EnableAndStartService enables and starts the etcd service
 func EnableAndStartService(service string) error {
 	if err := serviceEnable(service); err != nil {
 		return err
 	}
 	return serviceStart(service)
+}
+
+// DisableAndStopService disables and stops the etcd service
+func DisableAndStopService(service string) error {
+	if err := serviceDisable(service); err != nil {
+		return err
+	}
+	if err := serviceStop(service); err != nil {
+		return err
+	}
+	return resetFailed()
 }
