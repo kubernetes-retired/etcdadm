096d405b4fd8f6c8f21a96ac03a39544cb86acb7
Daniel Lipovetsky <daniel@platform9.com>
2018-08-02 17:43:03 -0700

    Implement init --snapshot

--- cmd/init.go
+++ cmd/init.go
@@ -8,6 +8,7 @@ import (
 	"github.com/platform9/etcdadm/binary"
 	"github.com/platform9/etcdadm/certs"
 	"github.com/platform9/etcdadm/constants"
+	"github.com/platform9/etcdadm/snapshot"
 
 	"github.com/platform9/etcdadm/service"
 	"github.com/spf13/cobra"
@@ -51,6 +52,11 @@ var initCmd = &cobra.Command{
 		if err = certs.CreatePKIAssets(&etcdAdmConfig); err != nil {
 			log.Fatalf("[certificates] Error: %s", err)
 		}
+		if etcdAdmConfig.Snapshot != "" {
+			if err := snapshot.Restore(&etcdAdmConfig); err != nil {
+				log.Fatalf("[snapshot] Error restoring snapshot: %v", err)
+			}
+		}
 		if err = service.WriteEnvironmentFile(&etcdAdmConfig); err != nil {
 			log.Fatalf("[configure] Error: %s", err)
 		}
@@ -79,4 +85,7 @@ func init() {
 	initCmd.PersistentFlags().StringVar(&etcdAdmConfig.ReleaseURL, "release-url", constants.DefaultReleaseURL, "URL used to download etcd")
 	initCmd.PersistentFlags().StringVar(&etcdAdmConfig.CertificatesDir, "certs-dir", constants.DefaultCertificateDir, "certificates directory")
 	initCmd.PersistentFlags().StringVar(&etcdAdmConfig.InstallBaseDir, "install-base-dir", constants.DefaultInstallBaseDir, "install base directory")
+	initCmd.PersistentFlags().StringVar(&etcdAdmConfig.Snapshot, "snapshot", "", "Etcd v3 snapshot file used to initialize member")
+	initCmd.PersistentFlags().BoolVar(&etcdAdmConfig.SkipHashCheck, "skip-hash-check", false, "Ignore snapshot integrity hash value (required if copied from data directory)")
+
 }
