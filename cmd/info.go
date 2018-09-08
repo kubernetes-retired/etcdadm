419a998a48c06269366a10a1494ba17be4a641d7
Daniel Lipovetsky <daniel@platform9.com>
2018-09-07 17:56:50 -0700

    cmd/info: Use new etcd package

--- cmd/info.go
+++ cmd/info.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/platform9/etcdadm/apis"
-	"github.com/platform9/etcdadm/util"
+	"github.com/platform9/etcdadm/constants"
+	"github.com/platform9/etcdadm/etcd"
 	"github.com/spf13/cobra"
 )
 
@@ -18,15 +20,26 @@ var infoCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalf("[defaults] Error: %s", err)
 		}
-		member, err := util.SelfMember(&etcdAdmConfig)
+
+		client, err := etcd.ClientForEndpoint(etcdAdmConfig.LoopbackClientURL.String(), &etcdAdmConfig)
 		if err != nil {
-			log.Fatalf("[info] Error requesting member information: %s", err)
+			log.Fatalf("[membership] Error requesting member information: %s", err)
 		}
-		memberJSON, err := json.MarshalIndent(&member, "", "  ")
+		ctx, cancel := context.WithTimeout(context.Background(), constants.DefaultEtcdRequestTimeout)
+		mresp, err := client.MemberList(ctx)
+		cancel()
+		if err != nil {
+			log.Fatalf("[membership] Error listing members: %v", err)
+		}
+		localMember, ok := etcd.MemberForID(mresp.Members, mresp.Header.MemberId)
+		if !ok {
+			log.Fatalf("[membership] Failed to identify local member in member list")
+		}
+		localMemberJSON, err := json.MarshalIndent(&localMember, "", "  ")
 		if err != nil {
 			log.Fatalf("[info] Error parsing member information: %s", err)
 		}
-		fmt.Println(string(memberJSON))
+		fmt.Println(string(localMemberJSON))
 	},
 }
 
