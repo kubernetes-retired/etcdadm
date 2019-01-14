62d720d0aa01cf3537beb51fd14a42e12f25ec2e
Justin SB <justinsb@google.com>
2019-01-14 11:21:10 -0500

    Much more functional etcd-manager-ctl

--- cmd/etcd-manager-ctl/main.go
+++ cmd/etcd-manager-ctl/main.go
@@ -17,46 +17,262 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
 	protoetcd "kope.io/etcd-manager/pkg/apis/etcd"
+	"kope.io/etcd-manager/pkg/backup"
 	"kope.io/etcd-manager/pkg/commands"
 )
+
+const DefaultEtcdVersion = "3.2.24"
+
+type Options struct {
+	MemberCount     int
+	EtcdVersion     string
+	BackupStorePath string
+}
+
+func (o *Options) SetDefaults() {
+	o.BackupStorePath = "/backups"
+}
 
 func main() {
 	flag.Set("logtostderr", "true")
 
-	memberCount := 1
-	flag.IntVar(&memberCount, "members", memberCount, "initial cluster size; cluster won't start until we have a quorum of this size")
-	backupStorePath := "/backups"
-	flag.StringVar(&backupStorePath, "backup-store", backupStorePath, "backup store location")
-	etcdVersion := "3.2.18"
-	flag.StringVar(&etcdVersion, "etcd-version", etcdVersion, "etcd version")
+	var o Options
+	o.SetDefaults()
+
+	flag.IntVar(&o.MemberCount, "member-count", o.MemberCount, "initial cluster size; cluster won't start until we have a quorum of this size")
+	flag.StringVar(&o.BackupStorePath, "backup-store", o.BackupStorePath, "backup store location")
+	flag.StringVar(&o.EtcdVersion, "etcd-version", o.EtcdVersion, "etcd version")
 
 	flag.Parse()
 
-	fmt.Printf("etcd-manager-ctl\n")
+	command := ""
+	args := flag.Args()
+	if len(args) == 0 {
+		command = "get"
+		args = []string{}
+	} else {
+		command = args[0]
+		args = args[1:]
+	}
 
-	if backupStorePath == "" {
-		fmt.Fprintf(os.Stderr, "backup-store is required\n")
+	ctx := context.Background()
+
+	if err := run(ctx, &o, command, args); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-
-	commandsStore, err := commands.NewStore(backupStorePath)
-	if err != nil {
-		glog.Fatalf("error initializing backup store: %v", err)
-	}
-
-	spec := &protoetcd.ClusterSpec{
-		MemberCount: int32(memberCount),
-		EtcdVersion: etcdVersion,
-	}
-	if err := commandsStore.SetExpectedClusterSpec(spec); err != nil {
-		glog.Fatalf("error building etcd controller: %v", err)
-	}
-
 	os.Exit(0)
+}
+
+func run(ctx context.Context, o *Options, command string, args []string) error {
+	switch command {
+	case "get":
+		return runGet(ctx, o)
+	case "configure-cluster":
+		return runInitCluster(ctx, o)
+	case "list-backups":
+		return runListBackups(ctx, o)
+	case "list-commands":
+		return runListCommands(ctx, o)
+	case "delete-command":
+		return runDeleteCommand(ctx, o, args)
+	case "restore-backup":
+		return runRestoreBackup(ctx, o, args)
+	default:
+		return fmt.Errorf("unknown command %q", command)
+	}
+}
+
+func GetBackupStore(o *Options) (backup.Store, error) {
+	if o.BackupStorePath == "" {
+		return nil, fmt.Errorf("backup-store is required\n")
+	}
+
+	commandStore, err := backup.NewStore(o.BackupStorePath)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing backup store: %v", err)
+	}
+	return commandStore, nil
+}
+
+func GetCommandStore(o *Options) (commands.Store, error) {
+	if o.BackupStorePath == "" {
+		return nil, fmt.Errorf("backup-store is required\n")
+	}
+
+	commandStore, err := commands.NewStore(o.BackupStorePath)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing commands store: %v", err)
+	}
+	return commandStore, nil
+}
+
+func runListBackups(ctx context.Context, o *Options) error {
+	backupStore, err := GetBackupStore(o)
+	if err != nil {
+		return err
+	}
+
+	backups, err := backupStore.ListBackups()
+	if err != nil {
+		return fmt.Errorf("error listing backups: %v", err)
+	}
+
+	for _, b := range backups {
+		fmt.Fprintf(os.Stdout, "%v\n", b)
+	}
+
+	return nil
+}
+
+func runListCommands(ctx context.Context, o *Options) error {
+	commandStore, err := GetCommandStore(o)
+	if err != nil {
+		return err
+	}
+
+	commands, err := commandStore.ListCommands()
+	if err != nil {
+		return fmt.Errorf("error listing commands: %v", err)
+	}
+
+	for _, c := range commands {
+		data := c.Data()
+		fmt.Fprintf(os.Stdout, "%s\n", proto.CompactTextString(&data))
+	}
+
+	return nil
+}
+
+func runDeleteCommand(ctx context.Context, o *Options, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("syntax: delete-command <backupname>")
+	}
+	commandID := args[0]
+
+	commandStore, err := GetCommandStore(o)
+	if err != nil {
+		return err
+	}
+
+	commands, err := commandStore.ListCommands()
+	if err != nil {
+		return fmt.Errorf("error listing commands: %v", err)
+	}
+
+	for _, c := range commands {
+		data := c.Data()
+		if strconv.FormatInt(data.Timestamp, 10) == commandID {
+			err := commandStore.RemoveCommand(c)
+			if err != nil {
+				return fmt.Errorf("error deleting command: %v", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func runRestoreBackup(ctx context.Context, o *Options, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("syntax: restore-backup <backupname>")
+	}
+	backupName := args[0]
+
+	commandStore, err := GetCommandStore(o)
+	if err != nil {
+		return err
+	}
+
+	// Start with current configuration
+	clusterSpec, err := commandStore.GetExpectedClusterSpec()
+	if err != nil {
+		return fmt.Errorf("error reading etcd cluster spec: %v", err)
+	}
+
+	if clusterSpec == nil {
+		return fmt.Errorf("cluster spec was not found - is this a new cluster?")
+	}
+
+	cmd := &protoetcd.Command{
+		RestoreBackup: &protoetcd.RestoreBackupCommand{
+			Backup:      backupName,
+			ClusterSpec: clusterSpec,
+		},
+	}
+
+	if err := commandStore.AddCommand(cmd); err != nil {
+		return fmt.Errorf("error writing command to store: %v", err)
+	}
+
+	fmt.Fprintf(os.Stdout, "added restore-backup command: %v\n", cmd)
+	return nil
+}
+
+func runInitCluster(ctx context.Context, o *Options) error {
+	commandStore, err := GetCommandStore(o)
+	if err != nil {
+		return err
+	}
+
+	// Start with current configuration
+	clusterSpec, err := commandStore.GetExpectedClusterSpec()
+	if err != nil {
+		return fmt.Errorf("error reading etcd cluster spec: %v", err)
+	}
+
+	if clusterSpec == nil {
+		clusterSpec = &protoetcd.ClusterSpec{}
+	}
+
+	// Populate values specified by user
+	if o.MemberCount != 0 {
+		clusterSpec.MemberCount = int32(o.MemberCount)
+	}
+	if o.EtcdVersion != "" {
+		clusterSpec.EtcdVersion = o.EtcdVersion
+	}
+
+	// Populate default values
+	if clusterSpec.EtcdVersion == "" {
+		clusterSpec.EtcdVersion = DefaultEtcdVersion
+	}
+	if clusterSpec.MemberCount == 0 {
+		clusterSpec.MemberCount = 1
+	}
+
+	if err := commandStore.SetExpectedClusterSpec(clusterSpec); err != nil {
+		return fmt.Errorf("error setting expected cluster spec: %v", err)
+	}
+
+	fmt.Fprintf(os.Stdout, "set cluster spec to %v\n", clusterSpec)
+
+	return nil
+}
+
+func runGet(ctx context.Context, o *Options) error {
+	commandStore, err := GetCommandStore(o)
+	if err != nil {
+		return err
+	}
+
+	clusterSpec, err := commandStore.GetExpectedClusterSpec()
+	if err != nil {
+		return fmt.Errorf("error reading etcd cluster spec: %v", err)
+	}
+
+	if clusterSpec == nil {
+		return fmt.Errorf("no spec found in store %q (consider calling etcd-manager configure-cluster)", o.BackupStorePath)
+	}
+	fmt.Fprintf(os.Stdout, "%v\n", clusterSpec)
+
+	return nil
 }
