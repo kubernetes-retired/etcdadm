c1cde76d770320576a055c5bbd43217f17aed128
Justin SB <justinsb@google.com>
2019-01-16 20:25:33 -0500

    Misc logging improvements
    Nothing substantial, just a few disambiguations and level changes.

--- etcd-manager/pkg/locking/info.go
+++ etcd-manager/pkg/locking/info.go
@@ -1,10 +1,18 @@
 package locking
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type LockInfo struct {
 	Holder    string `json:"owner"`
 	Timestamp int64  `json:"timestamp"`
+}
+
+// String implements Stringer
+func (l *LockInfo) String() string {
+	return fmt.Sprintf("owner=%s, timestamp=%d", l.Holder, l.Timestamp)
 }
 
 func (l *LockInfo) ToJSON() ([]byte, error) {
