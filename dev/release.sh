d91a05e678a2e64b0b2fa0b8b3235731ecc1d4b8
Justin SB <justinsb@google.com>
2019-01-24 23:21:01 -0500

    Mark release v3
    We sync up with the etcd release version to avoid confusion.

--- dev/release.sh
+++ dev/release.sh
@@ -2,5 +2,5 @@
 
 TODAY=`date +%Y%m%d`
 echo "# Run these commands to do a release"
-echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=1.0.${TODAY} make push"
+echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=3.0.${TODAY} make push"
 echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=latest make push"
