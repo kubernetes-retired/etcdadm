3c6ad74658b7023dd702f3cd974866dc5cbd8b17
Justin SB <justinsb@google.com>
2019-03-25 12:52:44 -0400

    Update release script to push tags
    So that it's clear which source release corresponds to a particular tag.

--- dev/release.sh
+++ dev/release.sh
@@ -4,3 +4,5 @@ TODAY=`date +%Y%m%d`
 echo "# Run these commands to do a release"
 echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=3.0.${TODAY} make push"
 echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=latest make push"
+echo "git tag 3.0.${TODAY}"
+echo "git push --tags ssh://git@github.com/kopeio/etcd-manager"
