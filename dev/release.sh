3a8cfb3c6510cb80c1fc12b4a8b0119a7e98ecc0
Marco Pracucci <marco@pracucci.com>
2019-07-03 17:29:15 +0200

    Added instructions and script to release etcd-manager-ctl binaries

--- dev/release.sh
+++ dev/release.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 TODAY=`date +%Y%m%d`
+VERSION="3.0.${TODAY}"
+
 echo "# Run these commands to do a release"
-echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=3.0.${TODAY} make push"
+echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=${VERSION} make push"
 echo "DOCKER_IMAGE_PREFIX=kopeio/ DOCKER_TAG=latest make push"
-echo "git tag 3.0.${TODAY}"
+echo "git tag ${VERSION}"
 echo "git push --tags ssh://git@github.com/kopeio/etcd-manager"
+echo "./dev/build-assets.sh ${VERSION}"
+echo "# Finally, create a new release on GitHub attaching binary assets at dist/${VERSION}/"
