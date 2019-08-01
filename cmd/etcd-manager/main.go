c80b735b28e537609eaa04e2970f73386cc17399
Srikanth <srikanth.hanagud@gmail.com>
2019-08-01 23:42:56 +0530

    Initial changes for DO vendor added

--- cmd/etcd-manager/main.go
+++ cmd/etcd-manager/main.go
@@ -44,6 +44,7 @@ import (
 	"kope.io/etcd-manager/pkg/volumes"
 	"kope.io/etcd-manager/pkg/volumes/aws"
 	"kope.io/etcd-manager/pkg/volumes/gce"
+	"kope.io/etcd-manager/pkg/volumes/do"
 )
 
 type stringSliceFlag []string
@@ -220,6 +221,16 @@ func RunEtcdManager(o *EtcdManagerOptions) error {
 
 			volumeProvider = gceVolumeProvider
 			discoveryProvider = gceVolumeProvider
+
+		case "do":
+			doVolumeProvider, err := do.NewDOVolumes(o.ClusterName, o.VolumeTags, o.NameTag)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+
+			volumeProvider = doVolumeProvider
+			discoveryProvider = doVolumeProvider
 
 		default:
 			fmt.Fprintf(os.Stderr, "unknown volume-provider %q\n", o.VolumeProviderID)
