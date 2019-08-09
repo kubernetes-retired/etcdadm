b2797258fc9d933f68205565621a386b6e9771e6
Srikanth <srikanth.hanagud@gmail.com>
2019-08-10 01:24:56 +0530

    Add glog to discovery and minor formatting

--- pkg/volumes/do/discovery.go
+++ pkg/volumes/do/discovery.go
@@ -17,10 +17,9 @@ limitations under the License.
 package do
 
 import (
-	//"fmt"
+	"fmt"
 
-	// "github.com/golang/glog"
-
+	"github.com/golang/glog"
 	"kope.io/etcd-manager/pkg/privateapi/discovery"
 	"kope.io/etcd-manager/pkg/volumes"
 )
@@ -31,15 +30,28 @@ var _ discovery.Interface = &DOVolumes{}
 func (a *DOVolumes) Poll() (map[string]discovery.Node, error) {
 	nodes := make(map[string]discovery.Node)
 
+	glog.V(2).Infof("Polling all DO Volumes")
+
 	allVolumes, err := a.findVolumes(false)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to discover any volumes: %s", err)
 	}
 
+	// Iterate over every volume. By the time this is called, the instance should have the volume mounted
+	// When mounting a volume to an instance, the internal DOVolume object's Info's Description is set to the name tag
+	// which would either be k8s.io/etcd/main OR k8s.io/etcd/events.
+	// This name tag is passed as a command parameter to this container and the same is updated in DOVolumes
+	// Verify if the volume we are iterating matches the nameTag.
+	// This is a HACK until we update KOPS to utlize tags that we can compare here.
 	instanceToVolumeMap := make(map[string]*volumes.Volume)
 	for _, v := range allVolumes {
 		if v.AttachedTo != "" {
 			if a.nameTag == v.Info.Description {
+
+				var myIP, _ = a.MyIP()
+
+				glog.V(2).Infof("Discovered a matching DO Volume for this instance with instanceid=%s volumename=%s nameTag=%s myIP=%s etcdName=%s", v.AttachedTo, v.ProviderID, a.nameTag, myIP, v.EtcdName)
+
 				instanceToVolumeMap[v.AttachedTo] = v
 
 				// We use the etcd node ID as the persistent identifier, because the data determines who we are
@@ -47,7 +59,6 @@ func (a *DOVolumes) Poll() (map[string]discovery.Node, error) {
 					ID: v.EtcdName,
 				}
 
-				var myIP, _ = a.MyIP()
 				node.Endpoints = append(node.Endpoints, discovery.NodeEndpoint{IP: myIP})
 				nodes[node.ID] = node
 			}
