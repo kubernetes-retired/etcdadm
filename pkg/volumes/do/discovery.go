6757a45a3206559a4c035af38555ea7c3c30da27
Srikanth <srikanth.hanagud@gmail.com>
2019-09-17 08:57:34 +0530

    Incorporate tagging feature for DO

--- pkg/volumes/do/discovery.go
+++ pkg/volumes/do/discovery.go
@@ -32,40 +32,30 @@ func (a *DOVolumes) Poll() (map[string]discovery.Node, error) {
 
 	glog.V(2).Infof("Polling all DO Volumes")
 
-	var mytag = a.ClusterName + "-" + a.nameTag
-
 	allVolumes, err := a.findVolumes(false)
 	if err != nil {
 		return nil, fmt.Errorf("failed to discover any volumes: %s", err)
 	}
 
-	// Iterate over every volume. By the time this is called, the instance should have the volume mounted
-	// When mounting a volume to an instance, the internal DOVolume object's Info's Description is set to the
-	// clustername_name tag
-	// which would either be k8s.io/etcd/main OR k8s.io/etcd/events.
-	// This name tag is passed as a command parameter to this container and the same is updated in DOVolumes
-	// Verify if the volume we are iterating matches the nameTag.
-	// This is a HACK until we update KOPS to utlize tags that we can compare here.
 	instanceToVolumeMap := make(map[string]*volumes.Volume)
 	for _, v := range allVolumes {
 		if v.AttachedTo != "" {
-			if mytag == v.Info.Description {
-
-				var myIP, _ = a.MyIP()
-
-				glog.V(2).Infof("Discovered a matching DO Volume for this instance with instanceid=%s volumename=%s nameTag=%s myIP=%s etcdName=%s", v.AttachedTo, v.ProviderID, a.nameTag, myIP, v.EtcdName)
-
-				instanceToVolumeMap[v.AttachedTo] = v
-
-				// We use the etcd node ID as the persistent identifier, because the data determines who we are
-				node := discovery.Node{
-					ID: v.EtcdName,
-				}
-
-				node.Endpoints = append(node.Endpoints, discovery.NodeEndpoint{IP: myIP})
-				nodes[node.ID] = node
-			}
+			instanceToVolumeMap[v.AttachedTo] = v
 		}
+	}
+
+	for _, volume := range instanceToVolumeMap {
+		var myIP, _ = a.MyIP()
+
+		glog.V(2).Infof("Discovered a matching DO Volume for this instance with instanceid=%s volumename=%s nameTag=%s myIP=%s etcdName=%s", volume.AttachedTo, volume.ProviderID, a.nameTag, myIP, volume.EtcdName)
+
+		// We use the etcd node ID as the persistent identifier, because the data determines who we are
+		node := discovery.Node{
+			ID: volume.EtcdName,
+		}
+
+		node.Endpoints = append(node.Endpoints, discovery.NodeEndpoint{IP: myIP})
+		nodes[node.ID] = node
 	}
 
 	return nodes, nil
